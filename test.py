8ab27eca26587b3d50fa3cb2895755d86d8839a9
Oscar Liu <25657114+oscarliu2020@users.noreply.github.com>
2024-02-12 13:06:12 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 import requests
-req=requests.get('https://icanhazip.com')
+req=requests.get('https://ifconfig.me/all',headers={'User-Agent':'Mozilla/5.0 (X11; U; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/114.0.5776.207 Chrome/114.0.5776.207 Safari/537.36'})
 print(req.text)
